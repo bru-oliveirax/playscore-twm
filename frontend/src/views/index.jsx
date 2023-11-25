56e43d2368587d8be40c00a8c2793269ca0421a4
kafb04 <87765508+kafb04@users.noreply.github.com>
2023-11-25 19:07:36 -0300

    home

--- frontend/src/views/index.jsx
+++ frontend/src/views/index.jsx
@@ -2,3 +2,4 @@ export { default as Error } from "./PageNotFound";
 export { default as Start } from "./StartPage";
 export { default as Login } from "./LoginPage";
 export { default as SignUp } from "./SignUpPage";
+export { default as Home } from "./Home";
