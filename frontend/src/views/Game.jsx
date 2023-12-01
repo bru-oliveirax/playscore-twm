4164ccbe30b5f2c58b6d006e6b0b664064d2a1a4
kafb04 <87765508+kafb04@users.noreply.github.com>
2023-11-30 23:23:37 -0300

    axios

--- frontend/src/views/Game.jsx
+++ frontend/src/views/Game.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Header, Pacman } from '../components/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,116 +1,43 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Header, Pacman } from '../components/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        setLoading(true)
-        setTimeout(() => {
-            setLoading(false)
-        }, 2000)
-    }, [])
+  useEffect(() => {
+    axios.get("http://localhost:8080/games")
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      });
+  }, []);
 
-    return (
-        loading ?
-            <Pacman />
-            :
-            <div className='App' style={{ overflowY: 'auto' }}>
-
-                <Header />
-                <div class="container-sm">
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Categoria
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Role-playing (RPG)</a></li>
-                            <li><a class="dropdown-item" href="#">Adventure</a></li>
-                            <li><a class="dropdown-item" href="#">Platform</a></li>
-                            <li><a class="dropdown-item" href="#">First Person Shooter (FPS)</a></li>
-                            <li><a class="dropdown-item" href="#">Multiplayer Online Battle Arena (MOBA)</a></li>
-                        </ul>
-                    </div>
-
-                    <br />
-
-                    <div class="row align-items-center">
-
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src={require("../images/SMW.jpg")} class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h4 class="card-title">SUPER MARIO WONDER</h4>
-                                    <h4 class="card-score">4.5 ★</h4>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src={require("../images/diablo4.jpg")} class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h4 class="card-title">DIABLO IV</h4>
-                                    <h4 class="card-score">4.9 ★</h4>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src={require("../images/ItTakesTwo.jpg")} class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h4 class="card-title">IT TAKES TWO</h4>
-                                    <h4 class="card-score">4.7 ★</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div className='App' style={{ overflowY: 'auto' }}>
+      {loading ? <Pacman /> : (
+        <>
+          <Header />
+          <div className="container-sm">
+            {/* O restante do seu código para renderizar os cards aqui */}
+            {data.map((games) => (
+              <div key={games.id} className="col">
+                <div className="card h-100">
+                  <img src={require(`../images/${games.imageURL}`)} className="card-img-top" alt={games.title} />
+                  <div className="card-body">
+                    <h4 className="card-title">{games.title}</h4>
+                    <h4 className="card-score">{games.total_score} ★</h4>
+                  </div>
                 </div>
-                
-                <br />
-
-                <div class="container text-center 2">
-                    <div class="row align-items-center">
-
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src={require("../images/SMW.jpg")} class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h4 class="card-title">SUPER MARIO WONDER</h4>
-                                    <h4 class="card-title">4.5 ★</h4>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src={require("../images/diablo4.jpg")} class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h4 class="card-title">DIABLO IV</h4>
-                                    <h4 class="card-title">4.9 ★</h4>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="card h-100">
-                                <img src={require("../images/ItTakesTwo.jpg")} class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h4 class="card-title">IT TAKES TWO</h4>
-                                    <h4 class="card-title">4.7 ★</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <br />
-                    <br />
-                </div>
-            </div>
-            
-
-    );
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
+
 export default Home;
